14edf5fda6289f6db84bf9af54ba753341945b92
KaralchukArtem <43133886+KaralchukArtem@users.noreply.github.com>
2020-03-23 21:51:51 +0300

    Revert "Revert "refactor""

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const Cinema = require('./routes/cinema-route');
-const CinemaModel = require('./models/cinema-model');
-const connect = require('./config/db-connect');
+const CinemaRoute = require('./routes/cinema-route');
+const { connect, Cinema } = require('./config/db-connect');
 
 const app = express();
 
@@ -15,29 +14,32 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req,res,next){
-    console.log('Use connect');
-    connect();
-    next();
+app.use(CinemaRoute);
+
+app.get('/cinemas', (req, res) => {
+  Cinema.find({}, (err, cinemas) => {
+    if (err) {
+      console.log(err);
+      res.send({ status: 'error', message: err.toString() })
+      return;
+    }
+    res.send({ status: 'ok', cinemas })
+  });
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello');
-});
-app.use('/Cinema', Cinema);
-app.get('/Cinema', (req, res) => {
-    CinemaModel.find({})
-    .exec(function(err, cinema){
-        if(err){
-            console.log('Пезда');
-        }else{
-            res.json(cinema);
-        }
-    });
-});
 
-app.listen(PORT, () =>{
+connect((err, client) => {
+  if (err) {
+    console.error(err);
+    return
+  }
+  client.db.collection("TEST_COLLECTION").find({}).toArray((err, data) => console.log(err, data));
+  app.locals.db = client.db;
+  app.listen(PORT, () => {
     console.log('Server start ' + PORT);
+  });
 });
+
+
 
 
