a4a599e56605ebf132204d1025451d881b04df47
Karalchuk Artuom <artuom00@inbox.ru>
2020-06-04 00:41:46 +0300

    feat: "done filter add film"

--- front-end-app/src/app/add-film/add-film.component.ts
+++ front-end-app/src/app/add-film/add-film.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Timetable } from 'src/models/cinema/timetable';
 import { HttpService } from 'src/services/http.service';
 import { CinemaModel } from 'src/models/cinema/cinema';
-import { Hall } from 'src/models/cinema/hall';
-
 
 @Component({
   selector: 'app-add-film',
@@ -17,53 +15,89 @@ export class AddFilmComponent implements OnInit {
   public modelCinema = new CinemaModel();
   public modelTimetable = new Timetable();
   public ex:Timetable;
+  public triggerContinuation: boolean[];
+
   constructor(private httpService:HttpService) {
-    this.httpService.getCinema().subscribe((data:any) => {
-      this.modelCinema = data.result[0];
-      console.log(this.modelCinema);
-  });
+      this.httpService.getCinema().subscribe((data:any) => {
+        this.modelCinema = data.result[0];
+        console.log(this.modelCinema);
+    });
   }
 
   ngOnInit(): void { }
 
   submit(){
+  this.triggerContinuation = [];
+  let exit:number = 0;
+  let triggerNumber:number = 0;
+
   this.modelCinema.hall.forEach(element => {
     if(element.nameHall == this.selectedValue){
       this.modelTimetable.hall = element;
       this.modelTimetable.film.IMDb = 0;
     }
   });
+
   this.modelCinema.timetable.forEach(element => {
     
-     let dateInputClient = this.modelTimetable.date.split('-');
-     let date = new Date(+dateInputClient[0], +dateInputClient[1], +dateInputClient[2]);
+     let dateClient = this.modelTimetable.date.split('-');
+     let dateInputClient = new Date(+dateClient[0], +dateClient[1], +dateClient[2]);
 
-     let dateTimetable = element.date.split('-');
-     let date2 = new Date(+dateTimetable[0],+dateTimetable[1],+dateTimetable[2])
-     
-    if(date.getDate() == date2.getDate() && element.hall.nameHall == this.selectedValue){
-      let timeTimetable = element.time.split(':');
-      let timeInputClient = this.modelTimetable.time.split(':');
+     let dateServer = element.date.split('-');
+     let dateServerTable = new Date(+dateServer[0],+dateServer[1],+dateServer[2])
 
-      date = new Date(date.getFullYear(),date.getMonth(),date.getDate(),+timeInputClient[0],+timeInputClient[1]);
-      console.log(date + " date");
-      date2 = new Date(date2.getFullYear(),date2.getMonth(),date2.getDate(),+timeTimetable[0],+timeTimetable[1]);
-      //...5 minute for a exit cinema
-      date2.setMinutes(date2.getMinutes() + +element.film.long + 5);
-      console.log(date2 + " date2");
-
-      console.log(date.getTime() + " time");
-      console.log(date2.getTime() + " time2");
-      
-      if(
-        date.getTime() > date2.getTime() &&
-        date.getTime() < date2.getTime()) console.log(true);
-      else console.log(false);
+     switch (element.hall.nameHall) {
+      case this.selectedValue:
+        if(dateInputClient.getDate() == dateServerTable.getDate()){
+          //...create function (element,modelTimetable)
+          let timeServerTable = element.time.split(':');
+          let timeInputClient = this.modelTimetable.time.split(':');
+    
+          dateInputClient = new Date(dateInputClient.getFullYear(),dateInputClient.getMonth(),dateInputClient.getDate(),+timeInputClient[0],+timeInputClient[1]);
+          dateServerTable = new Date(dateServerTable.getFullYear(),dateServerTable.getMonth(),dateServerTable.getDate(),+timeServerTable[0],+timeServerTable[1]);
+    
+          let dateServerTableClose = new Date();
+          let dateInputClientClose = new Date();
+    
+          dateServerTableClose.setFullYear(dateServerTable.getFullYear(),dateServerTable.getMonth(),dateServerTable.getDate());
+          dateServerTableClose.setTime(dateServerTable.getTime());
+          dateServerTableClose.setMinutes(dateServerTableClose.getMinutes() + +element.film.long);
+    
+          dateInputClientClose.setFullYear(dateInputClient.getFullYear(),dateInputClient.getMonth(),dateInputClient.getDate());
+          dateInputClientClose.setTime(dateInputClient.getTime());
+          dateInputClientClose.setMinutes(dateInputClientClose.getMinutes() + +this.modelTimetable.film.long);
+    
+          if((
+              dateServerTable.getTime() <= dateInputClient.getTime() &&
+              dateServerTableClose.getTime() <= dateInputClient.getTime()
+              )||(
+              dateServerTable.getTime() >= dateInputClientClose.getTime() &&
+              dateServerTableClose.getTime() >= dateInputClientClose.getTime()
+            )){this.triggerContinuation.push(true);} else {this.triggerContinuation.push(false);}
+        }else if(exit<1){
+            exit++;
+            this.httpService.postAdd(this.modelTimetable).subscribe((data:any) => {
+              this.ex=data.result;
+            });;
+        }
+        break;
     }
   });
-  // this.httpService.postAdd(this.modelTimetable).subscribe((data:any) => {
-  //   this.ex=data.result;
-  // });;
+
+  //...create function (triggerContinuation) "he is a [] in this submit()"
+  for (let i = 0; i < this.triggerContinuation.length; i++) {
+    if(this.triggerContinuation[i] == true){
+      triggerNumber++; console.log(triggerNumber)
+    }
+    if(triggerNumber == this.triggerContinuation.length){
+      console.log("Можно отправлять");
+      this.httpService.postAdd(this.modelTimetable).subscribe((data:any) => {
+        this.ex=data.result;
+      });;
+    }
+    else console.log("Неможно отправлять");
+  }
+
   console.log(this.modelTimetable);
   }
 
